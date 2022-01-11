85ab5b8125649ca1bb5f20c9f1edf75da34ed0c9
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-01-11 15:00:55 +0000

    first commit

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+let toDoItems = [];
+let thingsToDo = 0;
+
+const createToDo = (ev) => {
+    //prevent the page reloading on button press
+    ev.preventDefault();
+
+    //take the name value that has been input and save it to a variable
+    d
+}
