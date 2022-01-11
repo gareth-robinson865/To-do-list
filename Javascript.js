fc86897d6326f6b0d7062fec5d05c50882860431
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-01-11 15:52:55 +0000

    basic flow for creating the to do item

--- Javascript.js
+++ Javascript.js
@@ -6,5 +6,27 @@ const createToDo = (ev) => {
     ev.preventDefault();
 
     //take the name value that has been input and save it to a variable
-    d
+    let item = document.getElementById("to-do-entry").value;
+    console.log(item);
+
+    //reset the form to blank for the next input
+    document.forms[0].reset();
+
+    //push the to do item to the array
+    toDoItems.push(item);
+    console.log(toDoItems);
+
+    //create a new list item with the class "list-item"
+    let newListItem = document.createElement("li");
+    newListItem.setAttribute("class", "new-list-item");
+
+    //creating the text node to hold the information
+    let listText = document.createTextNode(item);
+
+    //appending the text node to the list element
+    newListItem.appendChild(listText);
+
+    //appending the new list item to the overall list
+    let itemList = document.getElementById("list");
+    itemList.appendChild(newListItem);
 }
